ba66874ece0e55891ebffae84f0f26131029eca0
shavelyananahit-hub <shavelyananahit@gmail.com>
2026-04-10 01:50:15 +0400

    Add files via upload

---
+++ Rotate.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int a[n][n]; 
+    for (int i=0;i<n;i++){
+        for (int j=0;j<n;j++){
+            cin>>a[i][j];
+        }
+    }
+
+    for (int i=0;i<n;i++) {
+        for (int j=i;j<n;j++) {
+            int temp=a[i][j];
+            a[i][j]=a[j][i];
+            a[j][i]=temp;
+        }
+    }
+
+    for (int i=0;i<n;i++) {
+        int l=0,r=n-1;
+        while (l<r){
+            int temp=a[i][l];
+            a[i][l]=a[i][r];
+            a[i][r]=temp;
+            l++;
+            r--;
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<a[i][j]<< " ";
+        }
+        cout<<endl;
+    }
+}
