6420d7ef2c1a71781c12131453c1193e904d5020
shavelyananahit-hub <shavelyananahit@gmail.com>
2026-04-10 01:40:05 +0400

    Add files via upload

---
+++ derivative.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double function(string f,double x) {
+    if (f=="square") return x*x;
+    if (f=="sin") return sin(x);
+    if (f=="exp") return exp(x);
+    if (f=="log") return log(x);
+    return x;
+}
+
+double deriv(string f,double x) {
+    if (f=="square") return 2*x;
+    if (f=="sin") return cos(x);
+    if (f=="exp") return exp(x);
+    if (f=="log") return 1/x;
+    return 1;
+}
+
+int main() {
+    int n;
+    cout<<"n = ";
+    cin>>n;
+
+    string f[100];
+    cout<<"Functions: ";
+    for (int i=0;i<n;i++) 
+    cin>>f[i];
+
+    double x;
+    cout<<"x = ";
+    cin>>x;
+
+    double val[100];
+    val[0]=x;
+
+    for (int i=n-1,j=1;i>=0;i--,j++) {
+        val[j]=function(f[i],val[j-1]);
+    }
+
+    double res=1;
+    for (int i=0;i<n;i++) {
+        res*=deriv(f[i],val[n-1-i]);
+    }
+    cout<<res;
+}
