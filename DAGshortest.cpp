09ab13829598fd7a8564a853b779f4ce1ff277f5
shavelyananahit-hub <shavelyananahit@gmail.com>
2026-05-06 08:48:22 +0400

    Add files via upload

---
+++ DAGshortest.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+const int N = 100;
+
+int a[N][N];
+bool used[N];
+int top[N], t = 0;
+
+int dist[N];
+int parent[N];
+
+int n = 5;
+
+void sort(int x) {
+    used[x] = true;
+
+    for (int i = 0; i < n; i++) {
+        if (a[x][i] != 0 && !used[i]) {
+            sort(i);
+        }
+    }
+
+    top[t++] = x;
+}
+
+int main() {
+
+    a[0][1] = 3;
+    a[0][2] = 25;
+    a[1][3] = 43;
+    a[2][3] = 10;
+    a[3][4] = 5;
+
+    for (int i = 0; i < n; i++)
+        if (!used[i])
+            sort(i);
+
+    for (int i = 0; i < n; i++) {
+        dist[i] = 1000000;
+        parent[i] = -1;
+    }
+
+    dist[0] = 0;
+
+    for (int i = t - 1; i >= 0; i--) {
+        int x = top[i];
+
+        if (dist[x] == 1000000) continue;
+
+        for (int j = 0; j < n; j++) {
+            if (a[x][j] != 0) {
+                if (dist[j] > dist[x] + a[x][j]) {
+                    dist[j] = dist[x] + a[x][j];
+                    parent[j] = x;
+                }
+            }
+        }
+    }
+
+    int end = 4;
+    cout << "Shortest length = " << dist[end] << endl;
+
+    int path[N], k = 0;
+
+    while (end != -1) {
+        path[k++] = end;
+        end = parent[end];
+    }
+
+    cout << "Path: ";
+    for (int i = k - 1; i >= 0; i--) {
+        cout << path[i] << " ";
+    }
+
+    return 0;
+}
